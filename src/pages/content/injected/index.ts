de472db0f2f2365664719df7dbfd21a1217c4c07
suzuki3jp <suzuki3.info@gmail.com>
2024-03-29 00:22:36 +0900

    feat: スクロールでボリュームを変更する

--- src/pages/content/injected/index.ts
+++ src/pages/content/injected/index.ts
@@ -77,6 +77,62 @@ class RightClickScroll {
 
 type RightClickScrollListener = () => void;
 
+class TwitchVideo {
+  private readonly PLAYER_QUERY = 'video';
+  private readonly VOLUMESLIDER_QUERY = '[id^="player-volume-slider"]';
+
+  private player: HTMLVideoElement;
+
+  constructor() {
+    this.player = this.getCurrentVideo();
+  }
+
+  public incrementVolume() {
+    const currentVolume = this.player.volume * 100;
+    const newVolume = currentVolume + 1;
+
+    this.changeVolume(newVolume);
+    return newVolume;
+  }
+  public decrementVolume() {
+    const currentVolume = this.player.volume * 100;
+    const newVolume = currentVolume - 1;
+
+    this.changeVolume(newVolume);
+    return newVolume;
+  }
+
+  public changeVolume(volume: number) {
+    const decimalVolume = volume / 100;
+    this.changeVideoVolume(decimalVolume);
+    this.changeVolumeSlider(decimalVolume);
+    this.changeVolumeLocalStrage(decimalVolume);
+  }
+
+  private changeVideoVolume(decimalVolume: number) {
+    this.player.volume = decimalVolume;
+  }
+
+  private changeVolumeSlider(decimalVolume: number) {
+    decimalVolume = parseFloat(decimalVolume.toFixed(2));
+    const slider = document.querySelector<HTMLInputElement>(this.VOLUMESLIDER_QUERY);
+    slider.value = `${decimalVolume}`;
+    slider.setAttribute('aria-valuenow', `${Math.round(decimalVolume * 100)}`);
+    slider.setAttribute('aria-valuetext', `${Math.round(decimalVolume * 100)}`);
+
+    const inputEvent = new Event('input', { bubbles: true, cancelable: true });
+    slider.dispatchEvent(inputEvent);
+  }
+
+  private changeVolumeLocalStrage(decimalVolume: number) {
+    localStorage.setItem('volume', `${decimalVolume}`);
+  }
+
+  private getCurrentVideo() {
+    return document.body.querySelector(this.PLAYER_QUERY);
+  }
+}
+
 (() => {
   console.log('Content scripts loaded');
   const currentLocation = new URL(location.href);
@@ -84,6 +140,7 @@ type RightClickScrollListener = () => void;
 
   if (locationWhitelist.includes(currentLocation.host)) {
     console.log('Twitch.tv now');
+    const video = new TwitchVideo();
     new RightClickScroll({
       start: () => {
         console.log('Scroll start');
@@ -93,9 +150,11 @@ type RightClickScrollListener = () => void;
       },
       scrollUp: () => {
         console.log('Scroll Up');
+        console.log(video.incrementVolume());
       },
       scrollDown: () => {
         console.log('Scroll Down');
+        console.log(video.decrementVolume());
       },
     });
   } else {
